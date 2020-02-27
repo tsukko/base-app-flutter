13040b98117b7d0f75f287b6ca688ae779714613
tsukko <tsukko@gmail.com>
2020-02-27 20:49:34 +0900

    comment to firebase code.

--- android/app/src/main/kotlin/jp/co/integrityworks/flutter_app123/Application.kt
+++ android/app/src/main/kotlin/jp/co/integrityworks/flutter_app123/Application.kt
@@ -1,18 +1,18 @@
 package jp.co.integrityworks.flutter_app123
 
-import io.flutter.app.FlutterApplication
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.PluginRegistry
-import io.flutter.plugins.GeneratedPluginRegistrant
-import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService
-
-class Application : FlutterApplication(), PluginRegistry.PluginRegistrantCallback {
-    override fun onCreate() {
-        super.onCreate()
-        FlutterFirebaseMessagingService.setPluginRegistrant(this)
-    }
-
-    override fun registerWith(registry: PluginRegistry) {
-        GeneratedPluginRegistrant.registerWith(registry as FlutterEngine)
-    }
-}
+//import io.flutter.app.FlutterApplication
+//import io.flutter.embedding.engine.FlutterEngine
+//import io.flutter.plugin.common.PluginRegistry
+//import io.flutter.plugins.GeneratedPluginRegistrant
+//import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService
+//
+//class Application : FlutterApplication(), PluginRegistry.PluginRegistrantCallback {
+//    override fun onCreate() {
+//        super.onCreate()
+//        FlutterFirebaseMessagingService.setPluginRegistrant(this)
+//    }
+//
+//    override fun registerWith(registry: PluginRegistry) {
+//        GeneratedPluginRegistrant.registerWith(registry as FlutterEngine)
+//    }
+//}
